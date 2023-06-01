e8c1a71e1daf77061d20da0ca4a84efc2ede3618
e-berman <ethanjamesberman@gmail.com>
2023-05-31 17:00:42 -0700

    add clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />;
+    </ClerkProvider>
+  );
+  
 };
 
 export default MyApp;
