799fe21832c3076bbe6d039e9818b9cd11d7fde6
e-berman <ethanjamesberman@gmail.com>
2023-07-06 22:19:04 -0700

    add analytics tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/dist/shared/lib/utils";
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
   return (
     <ClerkProvider {...pageProps} >
       <Component {...pageProps} />
+      <Analytics />
     </ClerkProvider>
   );
   
